5bd2ab0dbb798b044dc154a3b111e9cab0fde85d
Jonathan Santerre <jonathan.santerre@boreal-is.com>
2020-12-04 16:20:39 -0500

    fix react-refresh skipEnvCheck

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -459,9 +459,14 @@ module.exports = function (webpackEnv) {
                       },
                     },
                   ],
-                  isEnvDevelopment &&
-                    shouldUseReactRefresh &&
-                    require.resolve('react-refresh/babel'),
+                  [
+                    isEnvDevelopment &&
+                      shouldUseReactRefresh &&
+                      require.resolve('react-refresh/babel'), 
+                    { 
+                      skipEnvCheck: process.env.BABEL_ENV === 'test'
+                    }
+                  ]
                 ].filter(Boolean),
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
